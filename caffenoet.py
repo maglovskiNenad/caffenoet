20545ef2184d9d20b932861109eb493d45bdc2fe
Maglovski Nenad <79421496+maglovskiNenad@users.noreply.github.com>
2025-03-12 15:42:37 +0100

    first

---
+++ caffenoet.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.title("caffenoet")
+st.text("Coffee Market Watch – Track Coffee Prices in Real Time Welcome to Coffee Market Watch,"
+" your trusted source for the latest coffee market"
+" prices! Our platform provides up-to-date data on price fluctuations, market trends, and key"
+" insights that impact the coffee industry.\n- Real-time Data – "
+"Stay informed with live prices for Arabica and Robusta from major exchanges.\n- Charts & "
+"Analytics – Visualize price movements with interactive charts and"
+" historical data.\n- News & Forecasts – Keep up with market-driving factors such as weather"
+" conditions, production, and global demand.\n- Custom Alerts –"
+" Get notified about price changes to make smarter trading decisions.\nWhether you're an"
+" investor, trader, or a coffee enthusiast looking to understand "
+"market dynamics, Coffee Market Watch helps you stay ahead of the curve!Track Prices \n– "
+"Make Smarter Decisions!")
